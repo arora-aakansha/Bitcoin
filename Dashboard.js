8628c25ede871b038caa7010dc0b5ee3975bc205
aakansha <aakansha.a03@gmail.com>
2025-03-10 09:53:21 -0400

    fetch price

--- Dashboard.js
+++ Dashboard.js
@@ -3,5 +3,17 @@ import Plot from 'react-plotly.js';
 import axios from 'axios';
 
 const Bitocoin = () => {
+
+    cons [price, setPrice] = useState(0);
+    const [historicalData, setHistoricalData] = useState([]);
+
+    const fetchPrice = async () => {
+        try {
+          const response = await axios.get('http://localhost:5000/api/price');
+          setPrice(response.data.bitcoin_price);
+        } catch (error) {
+          console.error('Error fetching Bitcoin price:', error);
+        }
+      };
     
 }
